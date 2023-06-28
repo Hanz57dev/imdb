11f752e74f279baac776bc54ff05765ed8fb0e7f
Johan <test.vjhuntzz@gmail.com>
2023-06-28 13:32:16 +0700

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
+import Results from "@/components/Results";
 import Image from "next/image";
 
-export default function Home() {
-  return <h1 className="text-red-400">Home</h1>;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 60 * 60 * 24 * 3 } } // revalidate after 3 days
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
